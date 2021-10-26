82ecb1dd82b54facddcb54f5faa0169348668e14
ignacioarroyo7 <ignacioarroyo17@gmail.com>
2021-10-26 15:40:53 -0300

    se agregaron las validaciones de codigo, de numero, general, de url

--- js/validaciones.js
+++ js/validaciones.js
@@ -13,9 +13,20 @@ function validarCampoRequerido(input){
 //validar la parte de los numeros
 function validarNumeros(input){
     //creamos la expresion regular
-    let patron = /^[0-9]{1,4}$/; //expresion regular acepto numeros entre el 0 y el 9, y debo poner como minimo 1 y como maximo 4 digitos
+    let patron = /^[0-9]{1,4}$/; //expresion regular acepto numeros entre el 0 y el 9, y debo poner como minimo 1 y como maximo 4 digitos, $ cierre
 
-    if(patron.test(input.value)){
+    if(input.value.trim() != '' && patron.test(input.value.trim())){
+        input.className = 'form-control is-valid';
+        return true;
+    }else{
+        input.className = 'form-control is-invalid';
+        return false;
+    }
+}
+
+function validarUrl(input){
+    let patron = /^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/;
+    if(input.value.trim() != '' && input.value.trim()!= '' && patron.test(input.value.trim())){
         input.className = 'form-control is-valid';
         return true;
     }else{
@@ -27,12 +38,27 @@ function validarNumeros(input){
 
 function validarCodigo(input){
     if(input.value.trim() != '' && input.value.trim().length >= 3){
-        input.className += ' is-valid';
+        input.className ='form-control is-valid';
         return true;
     }else{
-        input.className += ' is-invalid'
+        input.className = 'form-control is-invalid';
         return false;
     }
+}
+
+function validarGeneral(e){ //para cuando apreto en el boton guardar, e o event, es que hago referencia al objeto event
+    //previene que recargue la pagina despues del submit
+    e.preventDefault();
+    alerta = document.querySelector('#msjAlert');
+    //if(true//false), pq todas las funciones devuelven un true o false
+    if(validarCodigo(codigo) && validarUrl(url) && validarCampoRequerido(producto) && validarCampoRequerido(descripcion) &&validarNumeros(cantidad)){
+        alerta.className='alert alert-danger mt-4 d-none';
+    }else{
+        
+        //aqui mostrar el alert html
+        alerta.className='alert alert-danger mt-4';
+    }
+
 }
 
 //esto es para manejar los eventos desde js para evitar injection HTML
@@ -42,11 +68,14 @@ let producto =document.querySelector('#producto');
 let descripcion = document.querySelector('#descripcion');
 let cantidad =document.querySelector('#cantidad');
 let url = document.querySelector('#url');
+let formProducto = document.querySelector('#formProducto');
+
 
 //le agregamos el evento
 //agarramos un objeto y le agregamos un escuchador de eventos
-codigo.addEventListener('blur',() => {validarCampoRequerido(codigo)});
+codigo.addEventListener('blur',() => {validarCodigo(codigo)});
 producto.addEventListener('blur',() => {validarCampoRequerido(producto)});
 descripcion.addEventListener('blur',() => {validarCampoRequerido(descripcion)});
 cantidad.addEventListener('blur',() => {validarNumeros(cantidad)});
-url.addEventListener('blur',() => {validarCampoRequerido(url)});
+url.addEventListener('blur',()=>{validarUrl(url)});
+formProducto.addEventListener('submit',validarGeneral);
