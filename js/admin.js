94876901ddcfd464ac01880b02c7bba5a076bbcd
ignacioarroyo7 <ignacioarroyo17@gmail.com>
2021-10-27 15:41:54 -0300

    se agregaron funciones como la de carga inicial, se realizo la carga al localStorage, la funcion agregar producto, guardarproducto y limpiar formulario

--- js/admin.js
+++ js/admin.js
@@ -5,6 +5,7 @@ import {
     validarNumeros,
     validarGeneral,
   } from "./validaciones.js";
+import { Producto } from "./productoClass.js";
 
 
 //esto es para manejar los eventos desde js para evitar injection HTML
@@ -15,6 +16,9 @@ let descripcion = document.querySelector('#descripcion');
 let cantidad =document.querySelector('#cantidad');
 let url = document.querySelector('#url');
 let formProducto = document.querySelector('#formProducto');
+let listaProductos = [];
+
+cargaInicial();
 
 //le agregamos el evento
 //agarramos un objeto y le agregamos un escuchador de eventos
@@ -31,8 +35,44 @@ function guardarProducto(e){
     if(validarGeneral()){
         //crear el producto
         console.log('producto creado');
+        agregarProducto();
     }else{
         //no hago nada, el usuario corrije
         console.log('corregir producto');
     }
+}
+
+function agregarProducto(){
+    //crea un objeto producto
+    let productoNuevo = new Producto(codigo.value, producto.value,descripcion.value, cantidad.value, url.value);
+    console.log(productoNuevo);
+
+    //cargar el objeto producto dentro del arreglo
+    listaProductos.push(productoNuevo);
+    //limpiar el formulario
+    limpiarFormulario();
+
+    //al arreglo de productos lo almacenamos en localstorage
+    localStorage.setItem('arregloProductos',JSON.stringify(listaProductos)); //stringify convierte el codigo a formato json
+
+    //mostrar un mensaje que indique que se agrego el producto correctamente
+
+
+    //agregar el objeto producto a una tabla
+}
+
+function limpiarFormulario(){
+    //reset limpia los value de los input
+    formProducto.reset();
+    codigo.className ='form-control';
+    producto.className  ='form-control';
+    descripcion.className  ='form-control';
+    cantidad.className  ='form-control';
+    url.className ='form-control';
+    
+}
+
+function cargaInicial(){
+    //traer los datos del localStorage
+    listaProductos = JSON.parse(localStorage.getItem('arregloProductos'))||[]; //metodo parse convierte de formato json a codigo 
 }
